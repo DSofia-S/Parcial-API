d2d25132ed8fa64d62c7c646d6b282d03dfd5d64
DSofia-S <zerausannad@gmail.com>
2025-09-04 14:49:18 -0500

    Se agrega el modelo Libro

--- api_app/models.py
+++ api_app/models.py
@@ -15,6 +15,8 @@ class Autor(models.Model):
         db_table='T001Autor'
         verbose_name='Autor'
         verbose_name_plural='Autores'
+
+# EDITORIAL
 class Editorial(models.Model):
     Id_Editorial = models.AutoField(primary_key=True, editable=False, db_column='T002IdEditorial')
     Nombre = models.CharField(max_length=100, db_column='T002Nombre')
@@ -29,3 +31,18 @@ class Editorial(models.Model):
         verbose_name='Editorial'
         verbose_name_plural='Editoriales'
         
+# LIBRO
+class Libro(models.Model):
+    Id_Libro = models.AutoField(primary_key=True, editable=False, db_column='T003IdLibro')
+    Titulo = models.CharField(max_length=100, db_column='T003Titulo')
+    Resumen = models.CharField(max_length=500, db_column='T003Resumen')
+    ISBN = models.CharField(max_length=13, unique=True, db_column='T003ISBN')
+    Año_Pub = models.DateField(db_column='T003Año')
+
+    def __str__(self):
+        return f"{self.Titulo}"
+
+    class Meta:
+        db_table='T003Titulo'
+        verbose_name='Titulo'
+        verbose_name_plural='Titulo'
